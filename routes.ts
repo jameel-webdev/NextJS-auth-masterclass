a6cdfc3a37bde8a5dac7e73a379a6405527c52ac
Jameel Ahmed <jameel.webdev@gmail.com>
2024-03-16 15:48:27 +0530

    ER

---
+++ routes.ts
@@ -0,0 +1,8 @@
+export const publicRoutes = ["/"];
+
+//These routes redirect login users to /settings
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+export const apiAuthPrefix = "/api/auth";
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
